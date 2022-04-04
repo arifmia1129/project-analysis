44abaf33fb89f5a4374f62c31bc2bed2cdc76255
arifvtti <arif.vtti@gmail.com>
2022-04-04 06:29:20 +0600

    make a custom hook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -28,14 +28,18 @@ const Home = () => {
                         How to manage your time and invest in your hereafter
 
                         How to be productive during Ramadan</p>
-                    <button className='my-5 border-4 px-5 py-2'>About This!</button>
+                    <button className='my-5 border-4 px-5 py-2 border-red-300 font-bold rounded-xl hover:bg-red-500 hover:text-white'><a href='https://www.amazon.com/Productive-Muslim-Where-Faith-Productivity/dp/1905837380' >About This!</a></button>
                 </div>
                 <div className='flex justify-center items-center '>
                     <img className='w-1/2 rounded-lg' src="content.jpg" alt="" />
                 </div>
             </div>
-            <div className=''>
+            <div className='my-10'>
+                <h3 className='text-3xl text-center font-bold text-blue-900'>Customer Reviews</h3>
 
+                <div>
+
+                </div>
             </div>
         </div>
     );
